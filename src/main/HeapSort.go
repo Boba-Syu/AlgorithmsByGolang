6312d44be6d95e7522e2ef77cc7e7b4fbea7e80b
Boba-Syu <1914808114@qq.com>
2019-05-15 17:16:50 +0800

    堆排序

---
+++ src/main/HeapSort.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{2, 1, 5, 7, 9, 0, 6, 4, 3, 8}
+	fmt.Println(a)
+	heapSort(a, 10)
+	fmt.Println(a)
+}
+
+func heapSort(a []int, N int) {
+	for i := N / 2; i >= 0; i-- { // 实现堆有序
+		sink(a, i, N)
+	}
+	fmt.Println(a)
+
+	for i := N - 1; i >= 0; i-- { // 排序
+		exch(a, 0, i)
+		sink(a, 0, i)
+	}
+}
+
+func sink(a []int, i, N int) { //将小的数字下沉
+	for i*2+1 < N {
+		j := i*2 + 1
+		if j < N-1 && a[j] < a[j+1] {
+			j++
+		}
+		if a[i] > a[j] {
+			break
+		}
+		exch(a, i, j)
+		i = j
+	}
+}
+
+func exch(a []int, i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
--- src/main/QuickSort.go
+++ src/main/QuickSort.go
@@ -8,17 +8,17 @@ import "fmt"
 func main() {
 	a := []int{2, 1, 5, 7, 9, 0, 6, 4, 3, 8}
 	fmt.Println(a)
-	sort(a, 0, 9)
+	quicoSort(a, 0, 9)
 	fmt.Println(a)
 }
 
-func sort(a []int, lo, hi int) { // 递归调用
+func quicoSort(a []int, lo, hi int) { // 递归调用
 	if lo >= hi {
 		return
 	}
 	j := partition(a, lo, hi)
-	sort(a, lo, j-1)
-	sort(a, j+1, hi)
+	quicoSort(a, lo, j-1)
+	quicoSort(a, j+1, hi)
 }
 
 func partition(a []int, lo, hi int) int { // 将a[lo+1]至a[hi]中所有小于a[lo]的数放左边, 大雨a[lo]的数放右边
